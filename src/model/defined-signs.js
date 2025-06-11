667190b4041f178a443dae7ad7a91209e201f437
izawartka <59137928+izawartka@users.noreply.github.com>
2025-06-11 19:04:04 +0200

    A lot of new sign definitions
    +Added definitions and icons for signs: W11, W11a, W11p, W27, W4, W6, W7, W8, W9, Z1

--- src/model/defined-signs.js
+++ src/model/defined-signs.js
@@ -1,6 +1,81 @@
 // remember to place sign_w1X before sign_w1 etc.
  
 const DefinedSigns = {
+    "sign_w27": {
+        "name": "W27",
+        "icon": "w27.svg",
+        "offsetY": -1.4364,
+        "text": "data",
+        "textSize": 0.22,
+        "textOffsetX": 1.89,
+        "textOffsetY": 1.2
+    },
+    "sign_w11p_400m": {
+        "name": "W11p 400m",
+        "icon": "w11p_2.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11p_200m": {
+        "name": "W11p 200m",
+        "icon": "w11p_1.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    // TODO: separate icons for W11a signs
+    "sign_w11a_400m": {
+        "name": "W11a 400m",
+        "icon": "w11_4.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11a_300m": {
+        "name": "W11a 300m",
+        "icon": "w11_3.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11a_200m": {
+        "name": "W11a 200m",
+        "icon": "w11_2.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11a_100m": {
+        "name": "W11a 100m",
+        "icon": "w11_1.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11_4": {
+        "name": "W11 400m",
+        "icon": "w11_4.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11_3": {
+        "name": "W11 300m",
+        "icon": "w11_3.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11_2": {
+        "name": "W11 200m",
+        "icon": "w11_2.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w11_1": {
+        "name": "W11 100m",
+        "icon": "w11_1.svg",
+        "offsetY": -3.3264,
+        "height": 1.5
+    },
+    "sign_w4": {
+        "name": "W4",
+        "icon": "w4.svg",
+        "offsetY": -1.4364
+    },
     "sign_w5": {
         "name": "W5",
         "icon": "w5.svg",
@@ -22,6 +97,41 @@ const DefinedSigns = {
         "icon": "w6.svg",
         "offsetY": -1.4364
     },
+    "sign_w7": {
+        "name": "W7",
+        "icon": "w7.svg",
+        "offsetY": -1.4364
+    },
+    "sign_w8_0": {
+        "name": "W8 (Next scenery)",
+        "icon": "w8.svg",
+        "offsetY": -1.4364,
+        "text": "static",
+        "staticText": "?",
+        "textSize": 0.14,
+        "textOffsetX": 1.89,
+        "textOffsetY": 0.9
+    },
+    "sign_w8": {
+        "name": "W8",
+        "icon": "w8.svg",
+        "offsetY": -1.4364,
+        "text": "data",
+        "textSize": 0.14,
+        "textOffsetX": 1.89,
+        "textOffsetY": 0.9
+    },
+    "sign_w9": {
+        "name": "W9",
+        "icon": "w9.svg",
+        "offsetY": -2.528064,
+        "height": 1.76
+    },
+    "sign_z1": {
+        "name": "Z1",
+        "icon": "z1.svg",
+        "offsetY": -1.4364
+    }
 };
 
 export default DefinedSigns;
