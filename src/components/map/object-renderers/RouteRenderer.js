5eca3d4558708ad433f06836b8440e251cd57c72
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-07-12 17:13:19 +0200

    Change position of the route arrow

--- src/components/map/object-renderers/RouteRenderer.js
+++ src/components/map/object-renderers/RouteRenderer.js
@@ -3,13 +3,14 @@ import AlwaysUpText from "../text/AlwaysUpText";
 
 export default function RouteRenderer(props) {
     const { object } = props;
-    const [x, y] = object.pos.toSVGCoords();
+    const [x, y] = object.end_center.toSVGCoords();
+    const angle = object.end_angle_rad * 180 / Math.PI;
 
     const offY = object.track_count === 2 ? -22 : -20;
     const arrowOffset = - object.track_offset - 37.8;
 
     return (
-        <g className="route" transform={`translate(${x}, ${y}) rotate(${object.rot.y})`}>
+        <g className="route" transform={`translate(${x}, ${y}) rotate(${angle})`}>
             <g transform={`rotate(90) translate(-80, ${arrowOffset})`}>
                 <ReactSVG
                     src={`${process.env.PUBLIC_URL}/assets/route.svg`}
@@ -21,7 +22,7 @@ export default function RouteRenderer(props) {
                 />
             </g>
             <AlwaysUpText
-                baseRot={object.rot.y}
+                baseRot={angle}
                 additionalRot={-90}
                 offsetX={object.track_offset}
                 reverseAnchor={true}
