f38dbe5cd350cb8558fedcf824a62304e13c7ca9
izawartka <59137928+izawartka@users.noreply.github.com>
2025-07-25 17:50:49 +0200

    Icons params fix
    *Fixed TrackHoverInfo icons to use JSX params instead of html attributes

--- src/icons/speed.js
+++ src/icons/speed.js
@@ -1,8 +1,8 @@
 export const SpeedIcon = ({width = 100, height = 100, color = '#aaa', strokeWidth = 3}) => (
     <svg width={width} height={height} version="1.1" viewBox="0 0 26.458 26.458" xmlns="http://www.w3.org/2000/svg">
-        <g fill="none" stroke={color} stroke-linecap="round">
-            <circle cx="13.112" cy="13.112" r="8.9958" stroke-width={strokeWidth}/>
-            <path d="m13.229 13.229 4.2063-3.3844" stroke-width={strokeWidth}/>
+        <g fill="none" stroke={color} strokeLinecap="round">
+            <circle cx="13.112" cy="13.112" r="8.9958" strokeWidth={strokeWidth}/>
+            <path d="m13.229 13.229 4.2063-3.3844" strokeWidth={strokeWidth}/>
         </g>
     </svg>
 );
