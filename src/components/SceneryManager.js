a624e092eb0f9c531235ce8bc4364cd31571cd64
izawartka <59137928+izawartka@users.noreply.github.com>
2025-07-27 14:48:48 +0200

    Added url params support
    +Added option to load a scenery by specifying its name in the "scenery" URL parameter

--- src/components/SceneryManager.js
+++ src/components/SceneryManager.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useRef } from 'react';
 import SceneryContext from '../contexts/SceneryContext';
 import { useZoomPanEmitter } from '../hooks/useZoomPubSub';
 import SceneryParser from '../model/scenery-parser';
@@ -12,6 +12,7 @@ export default function SceneryManager(props) {
     const [isLoading, setIsLoading] = useState(false);
     const [scenery, setScenery] = useState(null);
     const [sceneryListData, setSceneryListData] = useState(null);
+    const urlParamsLoaded = useRef(false);
     const { setCamera } = useZoomPanEmitter();
 
     const myFetch = useCallback(async (url, toJson) => {
@@ -37,10 +38,24 @@ export default function SceneryManager(props) {
 
     const loadCustomScenery = useCallback((file) => {
         return new Promise((resolve, reject) => {
-            setIsLoading(true);
+            let alreadyLoading;
+            setIsLoading(oldIsLoading => {
+                alreadyLoading = oldIsLoading;
+                return true;
+            });
+            if (alreadyLoading) {
+                console.warn("Already loading a scenery, ignoring new load request.");
+                return resolve(null);
+            }
+            
             resetHoveredTracksStack();
             setScenery(null);
             SceneryParserLog.clear();
+
+            const urlParams = new URLSearchParams(window.location.search);
+            urlParams.delete('scenery');
+            window.history.replaceState({}, '', urlParams.size  ? `${window.location.pathname}?${urlParams.toString()}` : window.location.pathname);
+
             const reader = new FileReader();
             reader.readAsText(file);
             reader.onload = (e) => {
@@ -86,9 +101,46 @@ export default function SceneryManager(props) {
         return await loadCustomScenery(file);
     }, [myFetch, loadCustomScenery]);
 
+    const findSceneryInList = useCallback((query) => {
+        if (!sceneryListData?.list || !Array.isArray(sceneryListData.list)) return null;
+
+        const liteCandidates = sceneryListData.list.filter(name => name === `${query}.lite.sc`);
+        if (liteCandidates.length > 0) return liteCandidates[0];
+
+        const scCandidates = sceneryListData.list.filter(name => name === `${query}.sc`);
+        if (scCandidates.length > 0) return scCandidates[0];
+
+        const candidates = sceneryListData.list.filter(name => name === query);
+        if(candidates.length > 0) return candidates[0];
+
+        return null;
+    }, [sceneryListData]);
+
+    const checkLoadUrlScenery = useCallback(async () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const sceneryQuery = urlParams.get('scenery');
+        if (!sceneryQuery) return;
+        const sceneryName = findSceneryInList(sceneryQuery);
+        if (!sceneryName) {
+            console.warn(`Scenery "${sceneryQuery}" not found in the list.`);
+            return;
+        }
+
+        await loadScenery(sceneryName);
+    }, [findSceneryInList, loadScenery]);
+
     useEffect(() => {
-        if(!Constants.sceneryFiles.fetchDisable) updateSceneryList();
+        if(Constants.sceneryFiles.fetchDisable) return;
+        updateSceneryList();
     }, [updateSceneryList]);
+
+    useEffect(() => {
+        if (!urlParamsLoaded.current) {
+            if (!sceneryListData) return;
+            urlParamsLoaded.current = true;
+            checkLoadUrlScenery();
+        }
+    }, [checkLoadUrlScenery, sceneryListData, urlParamsLoaded]);
 
     return (
         <SceneryContext.Provider value={{ isLoading, scenery, sceneryListData, loadScenery, loadCustomScenery }}>
