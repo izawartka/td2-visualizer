6b518821739bea0c9d543c785317571585de3098
izawartka <59137928+izawartka@users.noreply.github.com>
2025-07-31 22:52:43 +0200

    Added new signal signs
    +Added definitions for signal signs w2k, w2s, w26a, w26b
    *Renamed signal sign w21x to w21_led
    *Made w21 sign use a shared rect.svg icon and a static text

--- src/model/defs/defined-signal-signs.js
+++ src/model/defs/defined-signal-signs.js
@@ -9,6 +9,30 @@ const DefinedSignalSigns = {
         "icon": "w1.svg",
         "height": 1.28504,
     },
+    "w2k": {
+        "regex": /^w2k/,
+        "name": "W2 K",
+        "icon": "rect.svg",
+        "text": "K"
+    },
+    "w2s": {
+        "regex": /^w2s/,
+        "name": "W2 S",
+        "icon": "rect.svg",
+        "text": "S"
+    },
+    "w26a": {
+        "regex": /^(?:wsk_)?w26a/,
+        "name": "W26a",
+        "icon": "rect.svg",
+        "text": "P"
+    },
+    "w26b": {
+        "regex": /^(?:wsk_)?w26b/,
+        "name": "W26b",
+        "icon": "rect.svg",
+        "text": "D"
+    },
     "w18": {
         "regex": /^(?:wsk_)?w18(?:\D)?.*$/,
         "name": "W18",
@@ -29,17 +53,18 @@ const DefinedSignalSigns = {
         "name": "W24",
         "icon": "w24.svg",
     },
-    "w21x": {
+    "w21_led": {
         "regex": /^(?:wsk_)?w21_led_\d$/,
-        "name": "W21",
-        "icon": "w21x.svg",
+        "name": "W21 (LED)",
+        "icon": "rect.svg",
+        "text": "X"
     },
     "w21": {
         "regex": /^(?:wsk_)?w21(?!_led_)(?:\D)?.*$/,
         "name": "W21",
-        "icon": "w21.svg",
+        "icon": "rect.svg",
         "textRegex": /w21_(\d+)(?:v2)?/
-    },
+    }
 };
 
 export default DefinedSignalSigns;
